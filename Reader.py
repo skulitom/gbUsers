a21c620ff24fd29f9325848b27d67be2b9947719
Artem <artemskulimovskiy@gmail.com>
2017-07-17 11:56:21 +0100

    distance calc

--- Reader.py
+++ Reader.py
@@ -1,9 +1,18 @@
 import csv
 from User import *
+import datetime
 import statistics
 
 
-MAX_DISTANCE = 20
+PROP_SUM = 10
+CUT_POINT = 0.4
+
+MAX_DISTANCE = 100
+DISTANCE_PROP = 9
+WEEK_PROP = PROP_SUM - DISTANCE_PROP
+
+DISTANCE_LEN_PROP = 5
+DISTANCE_SPR_PROP = PROP_SUM - DISTANCE_LEN_PROP
 
 
 def main():
@@ -12,29 +21,78 @@ def main():
     userStats = [0, 0]
     with open('train_set.csv') as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
+        firstRow = False
         for row in readCSV:
             found = False
+            if not firstRow:
+                firstRow = True
+                continue
             for user in userList:
                 if user.userID == row[1]:
                     found = True
-                    user.appendUser(row[2], row[3], row[5], row[6], row[7], row[8], row[9], row[10])
+                    user.append_user(row[2], row[3], row[5], row[6], row[7], row[8], row[9], row[10])
             if not found:
                 userList.append(User(row[1], row[2], row[3], row[5], row[6], row[7], row[8], row[9], row[10]))
     for user in userList:
-        userAnalysis(user)
+        user_analysis(user)
         if user.gbUser == 0:
             userStats[0] += 1
         else:
             userStats[1] += 1
-    print("there are ", userStats[0], "good users and ", userStats[1],  " bad users")
+    print("there are", userStats[0], "good users and", userStats[1],  "bad users")
 
 
-def userAnalysis(user):
-    distanceAnalysis(user)
+def user_analysis(user):
+    distanceVal = distance_analysis(user)
+    weekVal = week_analysis(user)
+    result = (DISTANCE_PROP*distanceVal + WEEK_PROP*weekVal)/100
+    print(result)
+    if result > CUT_POINT:
+        user.gbUser = 1
+    else:
+        user.gbUser = 0
 
 
-def distanceAnalysis(user):
-    print("empty")
+def distance_analysis(user):
+    return DISTANCE_LEN_PROP * distance_length(user) + DISTANCE_SPR_PROP * distance_spread(user)
+
+
+def distance_length(user):
+    meanDistance = statistics.mean(user.distance)
+    if meanDistance >= MAX_DISTANCE:
+        return 1
+    else:
+        return meanDistance/MAX_DISTANCE
+
+
+def distance_spread(user):
+    meanDistance = statistics.mean(user.distance)
+    sdDistance = statistics.stdev(user.distance)
+    if sdDistance >= meanDistance:
+        return 1
+    else:
+        return sdDistance/meanDistance
+
+
+def week_analysis(user):
+    return week_spread(user) + week_quantity(user) + week_cluster(user)
+
+
+def week_spread(user):
+    return 0
+
+
+def week_quantity(user):
+    return 0
+
+
+def week_cluster(user):
+    return 0
+
+
+def get_datetime(timestamp):
+    return datetime.datetime.fromtimestamp(timestamp)
+
 
 if __name__ == '__main__':
     main()
--- User.py
+++ User.py
@@ -1,22 +1,22 @@
 class User:
     def __init__(self,  userID, sTimestamp, fTimestamp, timezoneOffset, duration, distance, sClusterID, fClusterID, isPredicted):
         self.userID = userID
-        self.sTimestamp = [sTimestamp]
-        self.fTimestamp = [fTimestamp]
-        self.timezoneOffset = [timezoneOffset]
-        self.duration = [duration]
-        self.distance = [distance]
+        self.sTimestamp = [int(float(sTimestamp))]
+        self.fTimestamp = [int(float(fTimestamp))]
+        self.timezoneOffset = [int(float(timezoneOffset))]
+        self.duration = [int(float(duration))]
+        self.distance = [int(float(distance))]
         self.sClusterID = [sClusterID]
         self.fClusterID = [fClusterID]
         self.isPredicted = [isPredicted]
         self.gbUser = 0
 
-    def appendUser(self, sTimestamp, fTimestamp, timezoneOffset, duration, distance, sClusterID, fClusterID, isPredicted):
-        self.sTimestamp.append(sTimestamp)
-        self.fTimestamp.append(fTimestamp)
-        self.timezoneOffset.append(timezoneOffset)
-        self.duration.append(duration)
-        self.distance.append(distance)
+    def append_user(self, sTimestamp, fTimestamp, timezoneOffset, duration, distance, sClusterID, fClusterID, isPredicted):
+        self.sTimestamp.append(int(float(sTimestamp)))
+        self.fTimestamp.append(int(float(fTimestamp)))
+        self.timezoneOffset.append(int(float(timezoneOffset)))
+        self.duration.append(int(float(duration)))
+        self.distance.append(int(float(distance)))
         self.sClusterID.append(sClusterID)
         self.fClusterID.append(fClusterID)
         self.isPredicted.append(isPredicted)
